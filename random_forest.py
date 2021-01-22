92340601e3fb70f38b5f6f4361ae48197ab0e186
Alisha Parveen <74652748+IEC-2020@users.noreply.github.com>
2021-01-22 22:06:32 +0100

    Update random_forest.py

--- random_forest.py
+++ random_forest.py
@@ -1,7 +1,9 @@
 ###RANDOM FOREST###
-rom sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
 rfc=RandomForestClassifier(n_estimators=100)
 rfc.fit(X_train, y_train)
 rfc_pred = rfc.predict(X_test)
+
+### ACCURACY ###
 accuracy_score(y_test, rfc_pred)
