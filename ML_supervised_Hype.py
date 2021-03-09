be92e8e6b34eff1fb0adfdb48a58930dbea47e23
Alisha Parveen <74652748+IEC-2020@users.noreply.github.com>
2021-03-09 15:58:19 +0100

    Update ML_supervised_Hype.py

--- ML_supervised_Hype.py
+++ ML_supervised_Hype.py
@@ -4,13 +4,40 @@
 Created on Wed Feb 24 20:28:18 2021
 @author: Dr. alishaparveen
 """
-
+###############################################################################################################################
+############################################################### Necessary Libraries ###########################################
+###############################################################################################################################
+#For loading the dataset#
 import pandas as pd
 import numpy as np
+# For Data preprocessing#
 from sklearn.preprocessing import Normalizer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+# Supervised machine learning algorithms#
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.neural_network import MLPClassifier
+# Hyper Parameter Tuning and Strata Cross validation#
+from sklearn.model_selection import StratifiedKFold 
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomSearchCV
+# Generation of Statistical report of classification problem#
+from sklearn.metrics import classification_report
+from statistics import mean, stdev 
+
+# Load the Dataset#
 df_imputed = pd.read_excel("Chi2.xlsx")
+
+###############################################################################################################################
+############################################################### Features Preprocessing ########################################
+###############################################################################################################################
+
 Y= df_imputed.iloc[:, 0:1].values
 Y= pd.DataFrame(data=Y)
 X= df_imputed.iloc[:, 1:32].values
@@ -20,38 +47,27 @@ y_test = pd.DataFrame(y_test)
 print('After Stratify Parameter is used ')
 print(y_train[0].value_counts())
 print(y_test[0].value_counts())
-
-###### Feature scaling #######
 scaler = StandardScaler()
 scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
-#### Check null values in column #####
-#X.isnull().sum()
-#X= pd.DataFrame(data=X)
-#print(Y[0].value_counts())
-
 ###############################################################################################################################
 ############################################################### 1. Logistics regression #######################################
 ###############################################################################################################################
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
-LR_classifier = LogisticRegression(C=1)
-LR_classifier.fit(X_train, y_train)
-LR_prediction = LR_classifier.predict(X_test)
-LR_report = classification_report(y_test, LR_prediction)
-print(LR_report)
 
-from sklearn.model_selection import StratifiedKFold 
-from statistics import mean, stdev 
+LogR_classifier = LogisticRegression(C=1)
+LogR_classifier.fit(X_train, y_train)
+LogR_prediction = LogR_classifier.predict(X_test)
+LogR_report = classification_report(y_test, LR_prediction)
+print(LogR_report)
 skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1) 
 lst_accu_stratified = [] 
 for train_index, test_index in skf.split(X, Y): 
     x_train_fold, x_test_fold = X_train, X_test 
     y_train_fold, y_test_fold = y_train, y_test
-    LR_classifier.fit(x_train_fold, y_train_fold) 
-    lst_accu_stratified.append(LR_classifier.score(x_test_fold, y_test_fold)) 
+    LogR_classifier.fit(x_train_fold, y_train_fold) 
+    lst_accu_stratified.append(LogR_classifier.score(x_test_fold, y_test_fold)) 
 print('List of possible accuracy:', lst_accu_stratified) 
 print('\nMaximum Accuracy That can be obtained from this model is:', 
       max(lst_accu_stratified)*100, '%') 
@@ -61,7 +77,6 @@ print('\nOverall Accuracy:',
       mean(lst_accu_stratified)*100, '%') 
 
 ######################################################### Hyperparameter tuning ##############################################
-from sklearn.model_selection import GridSearchCV
 penalty = ['l1', 'l2'] 
 C = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]
 #class_weight = [{1:0.5, 0:0.5}, {1:0.4, 0:0.6}, {1:0.6, 0:0.4}, {1:0.7, 0:0.3}]
@@ -70,7 +85,7 @@ param_grid = dict(penalty=penalty,
                   C=C,
                   #class_weight=class_weight,
                   solver=solver)
-LR_grid = GridSearchCV(estimator=LR_classifier,
+LR_grid = GridSearchCV(estimator=LogR_classifier,
                     param_grid=param_grid,
                     scoring='accuracy',
                     verbose=1,
@@ -81,10 +96,6 @@ grid_LR_report = classification_report(y_test, grid_LR_predictions)
 print(grid_LR_report)
 print('Best Score: ', LR_grid_result.best_score_)
 print('Best Params: ', LR_grid_result.best_params_)
-
-# Cross validation on  tunned model#
-from sklearn.model_selection import StratifiedKFold 
-from statistics import mean, stdev 
 skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1) 
 lst_accu_stratified = [] 
 for train_index, test_index in skf.split(X, Y): 
@@ -104,15 +115,12 @@ print('\nOverall Accuracy:',
 ############################################################### 2. SVC CLASSIFIER #############################################
 ###############################################################################################################################
 
-from sklearn.svm import SVC
+
 svclassifier = SVC(kernel='rbf')
 svclassifier.fit(X_train, y_train)
 svc_predictions = svclassifier.predict(X_test)
 svc_report = classification_report(y_test, svc_predictions)
 print(svc_report)
-read_to_excel
-from sklearn.model_selection import StratifiedKFold 
-from statistics import mean, stdev 
 skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1) 
 lst_accu_stratified = [] 
 for train_index, test_index in skf.split(X, Y): 
@@ -130,14 +138,12 @@ print('\nOverall Accuracy:',
 
 
 ##########################################################Hyperparameter tuning#################################################
-from sklearn.model_selection import GridSearchCV
 C = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]
 #class_weight = [{1:0.5, 0:0.5}, {1:0.4, 0:0.6}, {1:0.6, 0:0.4}, {1:0.7, 0:0.3}]
 kernel = ['linear', 'poly', 'rbf', 'sigmoid']
 gamma = ['scale', 'auto']
 param_grid = dict(C=C,
                   kernel=kernel, gamma=gamma)
-
 svc_grid = GridSearchCV(estimator=svclassifier,
                     param_grid=param_grid,
                     scoring='accuracy',
@@ -149,10 +155,6 @@ grid_svc_report = classification_report(y_test, grid_svc_predictions)
 print(grid_svc_report)
 print('Best Score: ', svc_grid_result.best_score_)
 print('Best Params: ', svc_grid_result.best_params_)
-
-#cross validation on tunned model#
-from sklearn.model_selection import StratifiedKFold 
-from statistics import mean, stdev 
 skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1) 
 lst_accu_stratified = [] 
 for train_index, test_index in skf.split(X, Y): 
@@ -172,7 +174,6 @@ print('\nOverall Accuracy:',
 ###############################################################################################################################
 ############################################################### 3. Decision Tree ##############################################
 ###############################################################################################################################
-from sklearn.tree import DecisionTreeClassifier
 DTclassifier = DecisionTreeClassifier(class_weight=None, max_depth=4,
                        max_features=None, max_leaf_nodes=None,
                        min_impurity_decrease=0.0, min_impurity_split=None,
@@ -183,9 +184,6 @@ DTclassifier.fit(X_train,y_train)
 DT_predictions = DTclassifier.predict(X_test)
 DT_report = classification_report(y_test, DT_predictions)
 print(DT_report)
-#4. Stratified Kfold validation
-from sklearn.model_selection import StratifiedKFold 
-from statistics import mean, stdev 
 skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1) 
 lst_accu_stratified = [] 
 for train_index, test_index in skf.split(X, Y): 
@@ -202,10 +200,7 @@ print('\nOverall Accuracy:',
       mean(lst_accu_stratified)*100, '%') 
 
 ##########################################################Hyperparameter tuning#################################################
-from sklearn.model_selection import GridSearchCV
 param_grid= {'max_leaf_nodes': list(range(2, 100)), 'min_samples_split': [2, 3, 4], 'criterion': ['gini', 'entropy']}
-#param_grid = dict(max_depth=max_depth,
-#                  criterion=criterion)
 DT_grid = GridSearchCV(estimator=DTclassifier,
                     param_grid=param_grid,
                     scoring='accuracy',
@@ -217,10 +212,6 @@ grid_DT_report = classification_report(y_test, grid_DT_predictions)
 print(grid_DT_report)
 print('Best Score: ', grid_DT_result.best_score_)
 print('Best Params: ', grid_DT_result.best_params_)
-
-#cross validation on tunned model#
-from sklearn.model_selection import StratifiedKFold 
-from statistics import mean, stdev 
 skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1) 
 lst_accu_stratified = [] 
 for train_index, test_index in skf.split(X, Y): 
@@ -240,15 +231,11 @@ print('\nOverall Accuracy:',
 ############################################################### 4. Naive Bayes ################################################
 ###############################################################################################################################
 
-from sklearn.naive_bayes import GaussianNB
 NBclassifier = GaussianNB()
 NBclassifier.fit(X_train,y_train)
 NB_predictions = NBclassifier.predict(X_test)
 NB_report = classification_report(y_test, NB_predictions)
 print(NB_report)
-#4. Stratified Kfold validation
-from sklearn.model_selection import StratifiedKFold 
-from statistics import mean, stdev 
 skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1) 
 lst_accu_stratified = [] 
 for train_index, test_index in skf.split(X, Y): 
@@ -269,16 +256,11 @@ print('\nOverall Accuracy:',
 ############################################################### 5. Random Forest ##############################################
 ###############################################################################################################################
 
-from sklearn.ensemble import RandomForestClassifier
 RFClassifier= RandomForestClassifier(n_estimators= 20, max_depth=3, random_state=0)
 RFClassifier.fit(X_train,y_train)
 RFC_predictions = RFClassifier.predict(X_test)
 RFC_report = classification_report(y_test, RFC_predictions)
 print(RFC_report)
-
-#4. Stratified Kfold validation
-from sklearn.model_selection import StratifiedKFold 
-from statistics import mean, stdev 
 skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1) 
 lst_accu_stratified = [] 
 for train_index, test_index in skf.split(X, Y): 
@@ -295,20 +277,12 @@ print('\nOverall Accuracy:',
       mean(lst_accu_stratified)*100, '%')
 
 ##########################################################Hyperparameter tuning#################################################
-from sklearn.model_selection import GridSearchCV
-#max_depth = [2,4,6,8,10,12,15,16]
-#criterion = ['gini', 'entropy']
-#n_estimators= [10,20,30,40,50,60]
 param_grid = { 
     'n_estimators': [100, 200, 500, 600],
     'max_features': ['auto', 'sqrt', 'log2'],
     'max_depth' : [4,5,6,7,8],
     'criterion' :['gini', 'entropy']
 }
-#param_grid = dict(max_depth=max_depth,
-#                  criterion=criterion,
-#                  n_estimators= n_estimators)
-
 RF_grid = GridSearchCV(estimator=RFClassifier,
                     param_grid=param_grid,
                     scoring='accuracy',
@@ -320,10 +294,6 @@ grid_RF_report = classification_report(y_test, grid_DT_predictions)
 print(grid_RF_report)
 print('Best Score: ', grid_RF_result.best_score_)
 print('Best Params: ', grid_RF_result.best_params_)
-
-#cross validation on tunned model#
-from sklearn.model_selection import StratifiedKFold 
-from statistics import mean, stdev 
 skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1) 
 lst_accu_stratified = [] 
 for train_index, test_index in skf.split(X, Y): 
@@ -343,16 +313,12 @@ print('\nOverall Accuracy:',
 ############################################################### 6. Gradient Boosting ##########################################
 ###############################################################################################################################
 
-from sklearn.ensemble import GradientBoostingClassifier
 GBClassifier= GradientBoostingClassifier(n_estimators=20, 
                                          learning_rate=1.0, max_depth=3, 
                                          random_state=0).fit(X_train, y_train)
 GBC_predictions = GBClassifier.predict(X_test)
 GBC_report = classification_report(y_test, GBC_predictions)
 print(GBC_report)
-#4. Stratified Kfold validation
-from sklearn.model_selection import StratifiedKFold 
-from statistics import mean, stdev 
 skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1) 
 lst_accu_stratified = [] 
 for train_index, test_index in skf.split(X, Y): 
@@ -388,10 +354,6 @@ grid_GBC_report = classification_report(y_test, grid_GBC_predictions)
 print(grid_GBC_report)
 print('Best Score: ', grid_GBC_result.best_score_)
 print('Best Params: ', grid_GBC_result.best_params_)
-
-#cross validation on tunned model#
-from sklearn.model_selection import StratifiedKFold 
-from statistics import mean, stdev 
 skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1) 
 lst_accu_stratified = [] 
 for train_index, test_index in skf.split(X, Y): 
@@ -412,18 +374,10 @@ print('\nOverall Accuracy:',
 ############################################################### 7. SGD Classifier #############################################
 ###############################################################################################################################
 
-from sklearn.linear_model import SGDClassifier
-from sklearn.pipeline import make_pipeline
-#SGDclassifier = make_pipeline(StandardScaler(),
-#                    SGDClassifier(penalty= 'elasticnet', max_iter=30, tol=1e-3, 
-#                                  alpha=0.05, loss="modified_huber")).fit(X_train, y_train)
 SGDclassifier=SGDClassifier().fit(X_train, y_train)
 SGD_predictions = SGDclassifier.predict(X_test)
 SGD_report = classification_report(y_test, SGD_predictions)
 print(SGD_report)
-#4. Stratified Kfold validation
-from sklearn.model_selection import StratifiedKFold 
-from statistics import mean, stdev 
 skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1) 
 lst_accu_stratified = [] 
 for train_index, test_index in skf.split(X, Y): 
@@ -440,20 +394,12 @@ print('\nOverall Accuracy:',
       mean(lst_accu_stratified)*100, '%')
 
 ############################################################## Hyperparameter tuning ###########################################
-#penalty=['l2', 'l1', 'elasticnet']
-#alpha = [0.001, 0.01,0.0001, 0.005,0.05,0.5]
-#max_iter= [50,100,150,200,250,300, 1000]
 param_grid = {
     'alpha': [1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3], # learning rate
-    'max_iter': [50,100,200,300,400,500, 1000], # number of epochs
-    #'loss': ['log'], # logistic regression,
+    'max_iter': [50,100,200,300,400,500, 1000], # number of epoch
     'penalty': ['l2','l1','elasticnet'],
     'n_jobs': [-1]
 }
-#param_grid = dict(penalty=penalty,
-#                  alpha=alpha,
-#                  #max_iter= max_iter)
-
 SGD_grid = GridSearchCV(estimator=SGDclassifier,
                     param_grid=param_grid,
                     scoring='accuracy',
@@ -465,10 +411,6 @@ grid_SGD_report = classification_report(y_test, grid_SGD_predictions)
 print(grid_SGD_report)
 print('Best Score: ', grid_SGD_result.best_score_)
 print('Best Params: ', grid_SGD_result.best_params_)
-
-#cross validation on tunned model#
-from sklearn.model_selection import StratifiedKFold 
-from statistics import mean, stdev 
 skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1) 
 lst_accu_stratified = [] 
 for train_index, test_index in skf.split(X, Y): 
@@ -488,15 +430,10 @@ print('\nOverall Accuracy:',
 ############################################################### 8. Neural Networks ############################################
 ###############################################################################################################################
 
-from sklearn.neural_network import MLPClassifier
 NNclassifier = MLPClassifier().fit(X_train, y_train)
 NN_predictions = NNclassifier.predict(X_test)
 NN_report = classification_report(y_test, NN_predictions)
 print(NN_report)
-
-#4. Stratified Kfold validation
-from sklearn.model_selection import StratifiedKFold 
-from statistics import mean, stdev 
 skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1) 
 lst_accu_stratified = [] 
 for train_index, test_index in skf.split(X, Y): 
@@ -535,10 +472,6 @@ grid_NN_report = classification_report(y_test, grid_NN_predictions)
 print(grid_NN_report)
 print('Best Score: ', grid_NN_result.best_score_)
 print('Best Params: ', grid_NN_result.best_params_)
-
-#cross validation on tunned model#
-from sklearn.model_selection import StratifiedKFold 
-from statistics import mean, stdev 
 skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1) 
 lst_accu_stratified = [] 
 for train_index, test_index in skf.split(X, Y): 
@@ -553,28 +486,3 @@ print('\nMinimum Accuracy:',
       min(lst_accu_stratified)*100, '%') 
 print('\nOverall Accuracy:', 
       mean(lst_accu_stratified)*100, '%')
-
-#print("Train Accuracy:",NNclassifier.score(X_train, y_train))
-#print("Test Accuracy:",NNclassifier.score(X_test, y_test))
-# Model performance on Training set#
-#from sklearn import metrics
-#from sklearn.metrics import confusion_matrix, classification_report
-#y_pred_train =NNclassifier.predict(X_train)
-#accuracy = metrics.accuracy_score(y_train, y_pred_train)
-#print("Accuracy: {:.2f}".format(accuracy))
-#cm=confusion_matrix(y_train,y_pred_train)
-#print('Confusion Matrix: \n', cm)
-#print(classification_report(y_train, y_pred_train))
-# Model performance on Test set#
-#y_pred_test=NNclassifier.predict(X_test)
-# Classification results on test set
-#accuracy = metrics.accuracy_score(y_test, y_pred_test)
-#print("Accuracy: {:.2f}".format(accuracy))
-#cm=confusion_matrix(y_test,y_pred_test)
-#print('Confusion Matrix: \n', cm)
-#print(classification_report(y_test, y_pred_test))
-# Model Performance after Cross Validation #
-#cv = KFold(n_splits=6, random_state=None, shuffle=True)
-#scores = cross_val_score(NNclassifier, X, Y, cv=cv, scoring='accuracy')
-#print(scores)
-#print(scores.mean())
