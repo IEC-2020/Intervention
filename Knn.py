d4a3c2fba9ee3d4d79781884cf8de86c6274fd34
Alisha Parveen <74652748+IEC-2020@users.noreply.github.com>
2021-04-19 10:33:11 +0200

    Delete Knn.py

--- Knn.py
+++
@@ -1,9 +0,0 @@
-#### Use Preprocessed dataset ######
-####### This is for KNN #################
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=20, stratify=y)
-knn = KNeighborsClassifier(3)
-knn.fit(X_train,y_train)
-print("Train score",knn.score(X_train,y_train),"%")
-print("Test score",knn.score(X_test,y_test),"%")
